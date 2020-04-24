ef24067afcde27be6926abe2c00f751409bf74af
themaleem <zizo9585@gmail.com>
2020-04-24 23:06:27 +0100

    diary can now get and post from user profile

--- users/views.py
+++ users/views.py
@@ -1,8 +1,11 @@
 from rest_framework import viewsets
 from .models import Profile,User
 from .serializers import UserSerializer,ProfileSerializer
+from shell.serializers import DiarySerializer
 from django_filters import rest_framework as dfilters  #using this instead of rest_framework.filters.FilterSet
 from rest_framework import filters #filters.FilterSet deprecated
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 class UserViewSet(viewsets.ModelViewSet):
     """
@@ -35,3 +38,27 @@ class ProfileViewSet(viewsets.ModelViewSet):
         'generated_username', #name of the profiles
         'username'
         )
+    
+    @action(detail=True,methods=["GET"])
+    def diaries(self,request,pk=None):
+        profile=self.get_object()
+        serializer_context = {
+            'request': request,
+        }
+        diaries=profile.diaries
+        serializer=DiarySerializer(diaries,context=serializer_context,many=True)
+        return Response(serializer.data,status=200)
+
+    @action(detail=True,methods=["POST"])
+    def diary(self,request,pk=None):
+        data=request.data
+        serializer_context = {
+            'request': request,
+        }
+        # data['owner'] = self.request.user
+        serializer=DiarySerializer(data=data,context=serializer_context)
+        if serializer.is_valid():
+            serializer.save(owner=self.request.user)
+            return Response(serializer.data,status=201)
+        return Response(serializer.errors,status=400)
+            
